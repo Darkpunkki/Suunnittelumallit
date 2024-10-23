7b034aa650bfa591c9c95a1c76ba29205f0f7cbf
Darkpunkki <jimi.pettila@metropolia.fi>
2024-10-23 15:16:46 +0300

    memento

---
+++ Assignments/src/memento/guistate/Memento.java
@@ -0,0 +1,34 @@
+package memento.guistate;
+
+import java.time.LocalDateTime;
+
+public class Memento implements IMemento {
+    private int[] options;
+    private boolean isSelected;
+    private LocalDateTime timestamp;
+
+    public Memento(int[] options, boolean isSelected) {
+        this.options = options.clone(); // Clone to prevent external modification
+        this.isSelected = isSelected;
+        this.timestamp = LocalDateTime.now();
+        System.out.println("Memento created");
+    }
+
+    public int[] getOptions() {
+        return options.clone(); // Return a clone to prevent external modification
+    }
+
+    public boolean isSelected() {
+        return isSelected;
+    }
+
+    @Override
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    @Override
+    public String toString() {
+        return "Saved at: " + timestamp.toString();
+    }
+}
